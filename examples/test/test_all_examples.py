8a6dd4a747bcffb51dac6613f926e68854863ce0
elenya-grant <116225007+elenya-grant@users.noreply.github.com>
2026-01-15 15:01:49 -0700

    Replace HOPP Examples that use PVWatts and PySAM Windpower with H2I Wrappers (#449)
    * updated example 03 hydrogenation doc to use PySAM wind model
    
    * added wind resource file for example 3 hydrogenation doc
    
    * updated example 3 hydrogenation to use h2i models
    
    * removed commented out hopp in example file
    
    ---------
    
    Co-authored-by: John Jasa <johnjasa11@gmail.com>

--- examples/test/test_all_examples.py
+++ examples/test/test_all_examples.py
@@ -241,7 +241,7 @@ def test_co2h_methanol_example(subtests):
 
     # Check levelized cost of methanol (LCOM)
     with subtests.test("Check CO2 Hydrogenation LCOM"):
-        assert pytest.approx(model.prob.get_val("methanol.LCOM")[0], rel=1e-6) == 1.381162
+        assert pytest.approx(model.prob.get_val("methanol.LCOM")[0], rel=1e-6) == 1.7555607442
 
 
 @pytest.mark.skipif(importlib.util.find_spec("mcm") is None, reason="mcm is not installed")
@@ -260,8 +260,8 @@ def test_doc_methanol_example(subtests):
     # Check levelized cost of methanol (LCOM)
     with subtests.test("Check CO2 Hydrogenation LCOM"):
         assert (
-            pytest.approx(model.prob.get_val("finance_subgroup_default.LCOM"), rel=1e-6)
-            == 2.58989518
+            pytest.approx(model.prob.get_val("finance_subgroup_default.LCOM")[0], rel=1e-4)
+            == 2.5252588
         )
 
 
