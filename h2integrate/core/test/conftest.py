bdd3f14e9c91b683d7964c6d67409f7aa7273bca
Hammond, Rob <13874373+RHammond2@users.noreply.github.com>
2026-03-12 16:33:52 -0700

    add module scoped example directory copy

--- h2integrate/core/test/conftest.py
+++ h2integrate/core/test/conftest.py
@@ -8,8 +8,10 @@ from h2integrate import EXAMPLE_DIR
 
 from test.conftest import (  # noqa: F401
     temp_dir,
+    temp_dir_module,
     temp_copy_of_example,
     pytest_collection_modifyitems,
+    temp_copy_of_example_module_scope,
 )
 
 
